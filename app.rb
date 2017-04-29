5a41ea6f13db7c74f6bdd3f778069f4d3d67e84c
Mined Minds <minds@wvchallenge.ad>
2017-04-29 10:01:55 -0400

    adding hangman file

---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require_relative "hangman.rb"
+	enable :sessions
+	get '/' do
+	erb :index
+end
+	post '/user_names' do
+	player1 = params[:user_1]
+	player2 = params[:user_2]
+	"Player 1 is #{player1} and Player 2 is #{player2}!"	
+	redirect '/password?player1=' + player1 +'&player2='+ player2
+end
+	get '/password' do
+	player1 = params[:player1]
+	player2 = params[:player2]
+	# "Player 1 is #{player1} and Player 2 is #{player2}!"
+	erb :password, locals: {p1_name: player1, p2_name: player2}
+end
+	post '/secretword' do
+	password = params[:word]
+	session[:game] = Hangman.new(password)
+	player1 = params[:player1]
+	player2 = params[:player2]
+	
+	redirect '/guessing'
+end
+	get '/guessing' do
+	player1 = params[:player1] 
+	player2 = params[:player2]
+	erb :guessing, locals: {p1_name: player1, p2_name: player2, blank: session[:game].correct_blank, array: session[:game].guessed,message: "pick a letter"}
+end
+	post '/guess' do
+	choice = params[:letter].upcase
+	player1 = params[:player1] 
+	player2 = params[:player2]
+	if session[:game].already_guessed(choice) == true
+		session[:game].make_move(choice)
+		session[:game].update_guessed(choice)
+		if session[:game].lose==true
+			redirect '/lose'
+		end
+	redirect "/guessing"
+	else
+		erb :guessing, locals: {p1_name: player1, p2_name: player2, blank: session[:game].correct_blank, array: session[:game].guessed,message: "thats already been picked"}
+	end
+end
+ get "/lose" do
+ 	player1 = params[:player1] 
+	player2 = params[:player2]
+ 	erb :lose, locals: {p1_name: player1, p2_name: player2, word: session[:game].name}
+ 	
+ end
---
+++ hangman.rb
@@ -0,0 +1,55 @@
+
+class Hangman
+	attr_accessor :name,:guessed,:correct_blank,:counter
+
+	def initialize(password_name)
+		@name = password_name.upcase
+		@guessed = []
+		@correct_blank = blank()
+		@counter= 8
+	end
+
+
+
+	def charcount
+		name.length
+	end
+	def blank()
+		Array.new(charcount,"_")
+	end
+	def correct_letter?(letter)
+		name.include?(letter)
+	end
+	def update_guessed(choice)
+		guessed.push(choice)
+	end
+	def correct_index(guessletter)
+		password=name.split ("")
+		p guessletter
+		p password
+		password.each_with_index do |letter,index_position|
+		p letter
+			if letter == guessletter
+				correct_blank[index_position] =guessletter
+				# p correct_blank
+			end
+	    end
+	end
+	def already_guessed(letter)
+		if guessed.include?(letter)
+			false 
+		else 
+			true
+		end
+	end
+	def make_move(choice)
+		if correct_letter?(choice) == true
+			correct_index(choice)
+		else
+			@counter -=1
+		end
+	end
+	def lose 
+		counter==0 
+	end
+end
